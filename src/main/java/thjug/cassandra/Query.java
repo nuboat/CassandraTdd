dabef6d838ca35b4573ed70495ca9b0c426053b6
nuboat <nuboat@gmail.com>
2015-08-28 22:54:34 +0700

    Query Result

---
+++ src/main/java/thjug/cassandra/Query.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright © 2015 Peerapat Asoktummarungsri.
+ *
+ * Attribution CC BY This license lets others distribute, remix, tweak, and
+ * build upon your work, even commercially, as long as they credit you for the
+ * original creation. This is the most accommodating of licenses offered.
+ * Recommended for maximum dissemination and use of licensed materials.
+ *
+ * http://creativecommons.org/licenses/by/3.0/
+ * http://creativecommons.org/licenses/by/3.0/legalcode
+ */
+package thjug.cassandra;
+
+import com.datastax.driver.core.ConsistencyLevel;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.SimpleStatement;
+import com.datastax.driver.core.Statement;
+import java.util.List;
+
+
+/**
+ *
+ * @author pasoktummarungsri
+ */
+public class Query {
+
+    private static final String LOCAL = "SELECT * FROM local;";
+
+    public List<Row> querySystem() {
+        final Statement stmt = new SimpleStatement(LOCAL);
+        stmt.setConsistencyLevel(ConsistencyLevel.ONE);
+
+        final Session s = Manager.getInstance().createSession("system");
+        final ResultSet r = s.execute(stmt);
+
+        return r.all();
+    }
+}
---
+++ src/test/java/thjug/cassandra/QueryNGTest.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright © 2015 Peerapat Asoktummarungsri.
+ *
+ * Attribution CC BY This license lets others distribute, remix, tweak, and
+ * build upon your work, even commercially, as long as they credit you for the
+ * original creation. This is the most accommodating of licenses offered.
+ * Recommended for maximum dissemination and use of licensed materials.
+ *
+ * http://creativecommons.org/licenses/by/3.0/
+ * http://creativecommons.org/licenses/by/3.0/legalcode
+ */
+package thjug.cassandra;
+
+import beforetest.EmbeddedCassandra;
+import com.datastax.driver.core.Row;
+import java.util.List;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+/**
+ *
+ * @author pasoktummarungsri
+ */
+public class QueryNGTest {
+
+    @BeforeClass
+    public void before() {
+        assert EmbeddedCassandra.isStart();
+    }
+
+    @Test
+    public void testQuerySystem() {
+        final Query q = new Query();
+        final List<Row> result = q.querySystem();
+
+        assert result.isEmpty() == false : "result should not empty";
+    }
+
+}
